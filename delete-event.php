547d2cd65b5a294f3b5da5610c78f96f01720c45
Jackmaa <valentingillot@gmail.com>
2025-01-27 10:37:00 +0100

    ♻️ Updated event deletion process to redirect to index page; added date input script to main page

--- delete-event.php
+++ delete-event.php
@@ -11,7 +11,6 @@ if (! isset($_SESSION["userid"])) {
 
 // Get the event ID and redirect page from the query parameters
 $id_event = $_GET['id'];
-$redirect = isset($_GET['redirect']) ? $_GET['redirect'] : 'events.php';
 
 // Include the database handler class
 include './dbh.class.php';
@@ -29,7 +28,7 @@ try {
     $req->execute();
 
     // Redirect to the specified page after deletion
-    header("Location: $redirect");
+    header("Location: index.php");
     exit();
 } catch (Exception $e) {
     // If there is an error, return it as a JSON response
